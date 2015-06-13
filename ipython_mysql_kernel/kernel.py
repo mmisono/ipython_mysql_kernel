8389df089ef05e9569abcdedb1fb751a5461dd7a
Masanori Misono <m.misono760@gmail.com>
2015-06-13 19:35:50 +0900

    connect MySQL server via pexpect's mysql process config file are read from ~/.ipython/mysql_config.json

--- ipython_mysql_kernel/kernel.py
+++ ipython_mysql_kernel/kernel.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python
 
+import json
+import os
+import signal
+
 from IPython.kernel.zmq.kernelbase import Kernel
+import pexpect
 
 class MySQLKernel(Kernel):
+    """MySQLKernel for IPython
+       this program use pexpect to interact with the mysql process
+       mysql configs are read from ~/.ipython/mysql_config.json
+       default configs are
+            mysql_config = {
+               "user"     : "root",
+               "host"     : "127.0.0.1",
+               "port"     : "3306",
+               "charset"  : "utf8"
+            }
+        you can set "password" in mysql_config.json if needed
+    """
     implementation = 'MySQL'
     implementation_version = '0.1'
     language = 'mysql'
@@ -10,14 +27,86 @@ class MySQLKernel(Kernel):
     language_info = {'mimetype': 'text/plain'}
     banner = "MySQL kernel"
 
-    def __init__(self, **kwargs):
-        Kernel.__init__(self, **kwargs)
+    mysql_setting_file = os.path.join(os.path.expanduser("~"),
+                                     ".ipython/mysql_config.json")
+    mysql_config = {
+       "user"     : "root",
+       "host"     : "127.0.0.1",
+       "port"     : "3306",
+       "charset"  : "utf8"
+    }
 
-    def do_execute(self, code, silent, store_history=True, user_expressions=None,
-                   allow_stdin=False):
+    def __init__(self, prompt="mysql> ", **kwargs):
+        Kernel.__init__(self, **kwargs)
+        self.prompt = prompt
+        self._start_process()
+
+    def _start_process(self):
+        if os.path.exists(self.mysql_setting_file):
+            with open(self.mysql_setting_file,"r") as f:
+                self.mysql_config.update(json.load(f))
+
+        # make childprocess interuptible by SIGINT
+        sig = signal.signal(signal.SIGINT, signal.SIG_DFL)
+        try:
+            if "password" in self.mysql_config:
+                self.ch = pexpect.spawn(
+                        'mysql -A -h {host} -P {port} -u {user} -p'.format(**self.mysql_config))
+            else:
+                self.ch = pexpect.spawn(
+                        'mysql -A -h {host} -P {port} -u {user}'.format(**self.mysql_config))
+        finally:
+            signal.signal(signal.SIGINT, sig)
+
+        if self.ch.echo:
+            self.ch.setecho(False)
+            self.ch.waitnoecho()
+
+        if "password" in self.mysql_config:
+            self.ch.expect('Enter password: ')
+            self.ch.sendline(self.mysql_config["password"])
+
+        self.ch.expect(self.prompt)
+
+    def do_execute(self, code, silent, store_history=True,
+                   user_expressions=None, allow_stdin=False):
+
+        code = code.replace("\n"," ").strip()
+
+        if not code:
+            return {'status': 'ok',
+                    'execution_count': self.execution_count,
+                    'payload': [],
+                    'user_expressions': {}}
+
+        if code[-1] != ";":
+            code = code + ";"
+
+        interrupted = False
+        try:
+            self.ch.sendline(code)
+            self.ch.expect(self.prompt)
+            output = self.ch.before
+        except KeyboardInterrupt:
+            self.sh.sendintr()
+            interrupted = True
+            output = self.sh.before
+        except pexpect.EOF:
+            output = self.ch.before + 'Restarting Process...'
+            self._start_process()
+
+        res = output.decode(self.mysql_config["charset"])
+        # remove echo string
+        # TODO: can't I disable echo?
+        if res.find(code) == 0:
+            res = res[len(code):]
+
         if not silent:
-            stream_content = {'name': 'stdout', 'text': code}
+            stream_content = {'name': 'stdout', 'text': res}
             self.send_response(self.iopub_socket, 'stream', stream_content)
+
+        if interrupted:
+            return {'status': 'abort', 'execution_count': self.execution_count}
 
         return {'status': 'ok',
                 'execution_count': self.execution_count,
