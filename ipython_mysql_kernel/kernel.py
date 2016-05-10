01d95aae04252456f7069eb76a253693eaccc422
Ubuntu <m.misono760@gmail.com>
2016-05-10 09:46:25 +0000

    oops

--- ipython_mysql_kernel/kernel.py
+++ ipython_mysql_kernel/kernel.py
@@ -380,10 +380,10 @@ class MySQLKernel(Kernel):
             self.ch.waitnoecho()
 
         if "password" in self.mysql_config:
-            self.ch.expect_exact('Enter password: ')
+            self.ch.expect('Enter password: ')
             self.ch.sendline(self.mysql_config["password"])
 
-        self.ch.expect_exact(self.prompt)
+        self.ch.expect(self.prompt)
 
     def do_execute(self, code, silent, store_history=True,
                    user_expressions=None, allow_stdin=False):
@@ -438,11 +438,11 @@ class MySQLKernel(Kernel):
         interrupted = False
         try:
             self.ch.sendline(mes)
-            self.ch.expect_exact(self.prompt)
+            self.ch.expect(self.prompt)
             output = self.ch.before
         except KeyboardInterrupt:
             self.ch.sendintr()
-            self.ch.expect_exact(self.prompt)
+            self.ch.expect(self.prompt)
             interrupted = True
             output = self.ch.before
         except pexpect.EOF:
@@ -521,10 +521,10 @@ class MySQLKernel(Kernel):
 
         try:
             self.ch.sendline(query.replace('\n',' '))
-            self.ch.expect_exact(self.prompt)
+            self.ch.expect(self.prompt)
         except KeyboardInterrupt:
             self.ch.sendintr()
-            self.ch.expect_exact(self.prompt)
+            self.ch.expect(self.prompt)
             return []
         except pexpect.EOF:
             #XXX: error handling
